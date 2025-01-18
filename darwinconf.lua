df97ac4f720ebae1d052c0ad25681c2a6134a229
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-18 13:35:47 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -17,4 +17,6 @@ if is_argv_present("--install_dependencies")  then
     os.execute("mkdir -p dependencies")
     os.execute("curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/SerjaoBerranteiroServer/archive/refs/tags/V3.zip -o dependencies/V3.zip")
     os.execute("unzip dependencies/V3.zip -d dependencies/serjao_berranteiro")    
+    os.execute("curl -L https://github.com/OUIsolutions/LuaDoTheWorld/archive/refs/tags/0.74.zip -o dependencies/0.74.zip")
+    os.execute("unzip dependencies/0.74.zip -d dependencies/lua_do_the_world")
 end 
