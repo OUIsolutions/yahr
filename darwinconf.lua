0fa972c8e42fe54aa6bc6a5135487a0be9cda09a
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-18 14:18:54 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -15,12 +15,11 @@ end
 if is_argv_present("--install_dependencies")  then
     dtw.remove_any("dependencies")
     os.execute("mkdir -p dependencies")
-    os.execute("curl -L https://github.com/OUIsolutions/CWebStudio/releases/download/v3.002/CWebStudio.h -o dependencies/CWebStudio.h")
-    os.execute("curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/SerjaoBerranteiroServer/archive/refs/tags/V3.zip -o dependencies/V3.zip")
-    os.execute("unzip dependencies/V3.zip -d dependencies/serjao_berranteiro")    
-    os.execute("curl -L https://github.com/OUIsolutions/LuaDoTheWorld/archive/refs/tags/0.74.zip -o dependencies/0.74.zip")
-    os.execute("unzip dependencies/0.74.zip -d dependencies/lua_do_the_world")
-    
+    os.execute("cd dependencies")
+    os.execute("git clone  https://github.com/SamuelHenriqueDeMoraisVitrio/SerjaoBerranteiroServer.git")
+    os.execute("curl -L https://github.com/OUIsolutions/LuaDoTheWorld/archive/refs/tags/0.74.zip -o 0.74.zip")
+    os.execute("unzip 0.74.zip -d lua_do_the_world")
+    os.execute("cd ..")
     
 end 
 
