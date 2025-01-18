04617b784b9866f21d2fae0e2d51664657853a5e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-18 13:40:28 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -20,3 +20,21 @@ if is_argv_present("--install_dependencies")  then
     os.execute("curl -L https://github.com/OUIsolutions/LuaDoTheWorld/archive/refs/tags/0.74.zip -o dependencies/0.74.zip")
     os.execute("unzip dependencies/0.74.zip -d dependencies/lua_do_the_world")
 end 
+
+--darwin.add_c_file("dependencies/serjao_berranteiro/src/main.c")
+
+local all = dtw.list_files_recursively("src",true)
+for i=1,#all do
+    darwin.add_lua_file(all[i])
+end 
+darwin.add_lua_code("main()")
+darwin.generate_c_executable_output({ output_name = "release/yahr.c" })
+
+if is_argv_present("--compile_linux") then 
+    os.execute("gcc -o release/yahr release/yahr.c ")
+end
+
+
+if is_argv_present("--compile_windows") then 
+    os.execute("x86_64-w64-mingw32-gcc -o release/yahr.exe release/yahr.c ")
+end
---
+++ src/main.lua
@@ -0,0 +1,5 @@
+
+
+function main()
+    print("aaa")
+end 
