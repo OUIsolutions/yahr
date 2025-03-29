20c4d20acbc9cc1d5129108b9b7e70977e1b96a9
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-28 23:57:19 -0300

    rebuilding reloader

--- assets/reloader.js
+++ assets/reloader.js
@@ -1,13 +1,19 @@
 let yahl_first_sha = null;
-
+let locked = false;
 setInterval(async function () {
+  if(locked) {
+    return ;
+  }
+  locked = true
   let sha = await fetch("/get_folder_sha");
   sha = await sha.text();
   if (yahl_first_sha === null) {
     yahl_first_sha = sha;
+    locked = false;
     return;
   }
   if (sha !== yahl_first_sha) {
     window.location.reload();
   }
+  locked = false;
 }, YAHL_DELAY);
